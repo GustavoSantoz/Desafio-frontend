9860255e44ae74b372e8ca6ea19f22a0acee6f28
Gustavo dos Santos <Guga_Vine@outlook.com>
2024-08-01 15:04:34 -0300

    feat: add a 404 error page

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,17 +1,16 @@
 import { BrowserRouter as Routers, Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home.tsx";
 import InventoryPage from "@/pages/Inventory.tsx";
+import NotFoundPage from "@/pages/Error";
 
-export default function Rotas() {
+export default function RouterConfig() {
   return (
-    <>
-      <Routers>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/inventory" element={<InventoryPage />} />
-          <Route path="/Produtos/:id" element={<h1>Produto</h1>} />
-        </Routes>
-      </Routers>
-    </>
+    <Routers>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/inventory" element={<InventoryPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Routers>
   );
 }
